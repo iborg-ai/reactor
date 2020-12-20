bf6fd3a9a1dfd1702cdf1d81509a63270aaf599f
Praveen Ravichandran <me@pravinba9495.dev>
2020-12-19 20:35:05 -0500

    Updates

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,14 +16,18 @@ export class HomeComponent implements OnInit {
     this.bridge.eeg.subscribe((eeg: any) => {
       if (eeg && eeg.rawEeg) {
         if (this.charts.length === 0) {
-          this.createNewChart(1);
-          this.createNewChart(2);
+          this.createNewChart(1, eeg.rawEeg.length);
+          this.createNewChart(2, eeg.rawEeg.length);
         } else {
           for (let chart of this.charts) {
             chart.data.labels.push("");
-            chart.data.datasets[0].data.push(eeg.rawEeg);
+            eeg.rawEeg.forEach((e, i) => {
+              chart.data.datasets[i].data.push(e);
+            });
             if (chart.data.datasets[0].data.length > 25) {
-              chart.data.datasets[0].data.shift();
+              eeg.rawEeg.forEach((e, i) => {
+                chart.data.datasets[i].data.shift();
+              });
               chart.data.labels.shift();
             }
             chart.update(0);
@@ -33,7 +37,7 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  createNewChart(index: number) {
+  createNewChart(index: number, channels) {
     const ctx = (<HTMLCanvasElement>(
       document.getElementById(`chart-${index}`)
     )).getContext("2d");
@@ -41,30 +45,41 @@ export class HomeComponent implements OnInit {
       type: "line",
       data: {
         labels: [],
-        datasets: [
-          {
-            label: "Ch1",
-            data: [],
-          },
-        ],
+        datasets: JSON.parse(JSON.stringify(new Array(channels))).map(
+          (_, i) => {
+            return {
+              label: `Ch${i + 1}`,
+              data: [],
+              backgroundColor: "rgba(0, 0, 0, 0.0)",
+              // borderColor: 'rgba(0, 0, 0, 0.5)',
+              borderWidth: 1,
+              pointRadius: 0,
+            };
+          }
+        ),
       },
       options: {
+        plugins: {
+          colorschemes: {
+            scheme: "brewer.Paired12",
+          },
+        },
         scales: {
           yAxes: [
             {
               ticks: {
-                min: -1500,
-                max: 1500,
-                beginAtZero: false,
-                stepSize: 20,
+                // min: -4500,
+                // max: 4500,
+                // beginAtZero: true,
+                // stepSize: 10,
               },
             },
           ],
           xAxes: [
             {
               ticks: {
-                beginAtZero: true,
-                stepSize: 0.1,
+                // beginAtZero: true,
+                // stepSize: 0.1,
                 maxTicksLimit: 5,
               },
             },
